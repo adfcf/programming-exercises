5ac98ffd3812039000eac594012c7141e33964c3
adfcf <adfcf@aluno.ifnmg.edu.br>
2023-06-05 15:11:39 -0300

    Added r1.rs

---
+++ rust/r1.rs
@@ -0,0 +1,16 @@
+// Rust 1
+// Choose 5 arithmetic operations, format their results into a string and print it.
+
+fn main() {
+
+    let m11 = 1 * 1;
+    let m22 = 2 * 2;
+    let m33 = 3 * 3;
+    let m44 = 4 * 4;
+    let m55 = 5 * 5;
+
+    let s = format!("1x1 = {m11}, 2x2 = {m22}, 3x3 = {m33}, 4x4 = {m44}, 5x5 = {m55}");
+
+    println!("Formatted string: {s}");
+
+}
